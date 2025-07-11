272a7470fc6e76d5072fc20b02ddb697aef77ef8
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-07-11 03:43:33 -0300

    add fibsRec function, refactor fibs function

--- fibbonaci.js
+++ fibbonaci.js
@@ -1,4 +1,8 @@
 function fibs(n) {
+  // if n is less than or equal to 0 we return []
+  if (n <= 0) {
+    return [];
+  }
   // if n is 1 we return [0]
   if (n === 1) {
     return [0];
@@ -8,21 +12,35 @@ function fibs(n) {
     return [0, 1];
   }
 
-  // set the previous and next numbers and their sum
-  // then append it to the array & update all 3 variables
-  // with the new values
-  let first = 0;
-  let next = 1;
-  let sum = first + next;
-  let array = [first, next];
-  for (let i = 0; i < n - 2; i++) {
-    array.push(sum);
-    first = next;
-    next = sum;
-    sum = first + next;
+  // create the array and iteratively add the sum of the
+  // previous two elements
+  let array = [0, 1];
+  for (let i = 2; i < n; i++) {
+    array.push(array[i - 1] + array[i - 2]);
   }
   return array;
 }
 
+function fibsRec(n) {
+  // if n is 0 we return []
+  if (n === 0) {
+    return [];
+  }
+  // if n is 1 we return [0]
+  if (n === 1) {
+    return [0];
+  }
+  // if n is 2 we return [0, 1]
+  if (n === 2) {
+    return [0, 1];
+  }
+
+  // create the array with the first 2 elements recursively
+  // then add the sum to the array
+  let fibArray = fibsRec(n - 1);
+  fibArray.push(fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]);
+  return fibArray;
+}
+
 console.log(fibs(8));
-console.log(fibs(6));
+console.log(fibsRec(8));
